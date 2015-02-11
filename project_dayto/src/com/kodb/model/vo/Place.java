153b3a6fd57aac73327bbb8f356f0f6b298ea54f
Administrator <Administrator@WIN-KW007>
2015-02-11 14:10:33 +0900

    place추가

---
+++ project_dayto/src/com/kodb/model/vo/Place.java
@@ -0,0 +1,68 @@
+package com.kodb.model.vo;
+
+import java.util.Date;
+
+public class Place {
+
+	private int placeId;
+	private String placeName;
+	private String placeTel;
+	private String placeCategory;	
+	private String placeAddr;
+	
+	public Place() {
+		super();
+	}
+
+	public int getPlaceId() {
+		return placeId;
+	}
+
+	public void setPlaceId(int placeId) {
+		this.placeId = placeId;
+	}
+
+	public String getPlaceName() {
+		return placeName;
+	}
+
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+
+	public String getPlaceTel() {
+		return placeTel;
+	}
+
+	public void setPlaceTel(String placeTel) {
+		this.placeTel = placeTel;
+	}
+
+	public String getPlaceCategory() {
+		return placeCategory;
+	}
+
+	public void setPlaceCategory(String placeCategory) {
+		this.placeCategory = placeCategory;
+	}
+
+	public String getPlaceAddr() {
+		return placeAddr;
+	}
+
+	public void setPlaceAddr(String placeAddr) {
+		this.placeAddr = placeAddr;
+	}
+
+
+	@Override
+	public String toString() {
+		return "place [placeId=" + placeId + ", placeName=" + placeName 
+				+ ", placeTel=" + placeTel + ", placeCategory=" + placeCategory 
+				+ ", placeAddr=" + placeAddr+ "]";
+	}
+	
+	
+	
+	
+}
